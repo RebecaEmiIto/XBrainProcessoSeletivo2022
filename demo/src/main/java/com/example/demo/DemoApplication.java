113c89b97ecd77ddce64277fffb7344e8eb54ea1
Rebeca <emi.ito@edu.unifil.br>
2022-12-16 15:10:12 -0300

    Trabalhando com Mapping

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ComponentScan("com.text.demo.controller")
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
+	public String greeting() {
+		return "Hello World!";
 	}
 
 }
